0dc7f50d61bde514d9ba517e29d0fcdb56e7b51b
Konstantin Makarchev <kostya27@gmail.com>
2013-01-02 19:31:38 +0400

    # refactor http checker

--- lib/eye/checker/http.rb
+++ lib/eye/checker/http.rb
@@ -5,6 +5,8 @@ class Eye::Checker::Http < Eye::Checker
   # ex: {:type => :http, :every => 5.seconds, :times => 1,
   #  :url => "http://localhost:3000/", :kind => :success, :pattern => /OK/, :timeout => 3.seconds
 
+  params :url, :pattern, :kind, :timeout, :open_timeout, :read_timeout
+
   def check_name
     "http"
   end
@@ -12,16 +14,16 @@ class Eye::Checker::Http < Eye::Checker
   def initialize(*args)
     super
 
-    @uri = URI.parse(options[:url])
-    @pattern = options[:pattern]
-    @kind = case options[:kind]
-              when Fixnum then Net::HTTPResponse::CODE_TO_OBJ[options[:kind].to_s]
-              when String, Symbol then Net.const_get("HTTP#{options[:kind].to_s.camelize}") rescue Net::HTTPSuccess
+    @uri = URI.parse(url)
+    @pattern = pattern
+    @kind = case kind
+              when Fixnum then Net::HTTPResponse::CODE_TO_OBJ[kind]
+              when String, Symbol then Net.const_get("HTTP#{kind.to_s.camelize}") rescue Net::HTTPSuccess
             else
               Net::HTTPSuccess
             end
-    @open_timeout = (options[:open_timeout] || options[:timeout] || 5).to_i
-    @read_timeout = (options[:read_timeout] || options[:timeout] || 30).to_i
+    @open_timeout = (open_timeout || timeout || 5).to_i
+    @read_timeout = (read_timeout || timeout || 30).to_i
 
     @session = Net::HTTP.new(@uri.host, @uri.port)
     if @uri.scheme == 'https'
