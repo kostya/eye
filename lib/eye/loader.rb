a913d22cbfa18140db689ca7edc8f82260ce8e92
Konstantin Makarchev <kostya27@gmail.com>
2013-01-04 15:46:50 +0400

    # dependencies

--- lib/eye/loader.rb
+++ lib/eye/loader.rb
@@ -1,13 +1,13 @@
 # mini bundler, for embedded server gem installation
 
-gem 'celluloid'
-gem 'celluloid-io'
+gem 'celluloid',     '~> 0.12'
+gem 'celluloid-io',  '~> 0.12'
 gem 'nio4r'
 gem 'facter'
 gem 'timers'
 
 gem 'state_machine'
-gem 'activesupport'
+gem 'activesupport', '~> 3.2'
 
 gem 'i18n' # for as
 gem 'multi_json' # for as
