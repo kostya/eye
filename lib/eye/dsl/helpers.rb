a6b385fdf6117d6e46cb8dbc36f7e650b016b416
Konstantin Makarchev <kostya27@gmail.com>
2013-02-02 01:16:47 +0400

    # dsl helpers

--- lib/eye/dsl/helpers.rb
+++ lib/eye/dsl/helpers.rb
@@ -1,3 +1,12 @@
+
+# Dsl Helpers 
+
+# current eye parsed config path
+def current_config_path
+  Eye.parsed_filename && File.symlink?(Eye.parsed_filename) ? File.readlink(Eye.parsed_filename) : Eye.parsed_filename
+end
+
+# host name
 def hostname
   Eye::System.host  
 end
