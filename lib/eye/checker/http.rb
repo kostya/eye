d4411dd09b24bc3233911f8bea5062fea86065ed
orangeudav <orangeudav@gmail.com>
2013-03-12 15:20:18 +0400

    Revert "fix http checker"
    This reverts commit 53237fc72b27cf10949f44b9f29269561245ff4c.

--- lib/eye/checker/http.rb
+++ lib/eye/checker/http.rb
@@ -68,7 +68,7 @@ class Eye::Checker::Http < Eye::Checker
 
     if @pattern
       if @pattern.is_a?(Regexp)
-        @pattern.match(value[:result].body)
+        @pattern === value[:result].body
       else
         value[:result].body.include?(@pattern.to_s)
       end
