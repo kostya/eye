fbf18a2540d4123b24efe46a6c04412e4417361c
Konstantin Makarchev <kostya27@gmail.com>
2013-03-29 02:07:55 +0400

    # remove randomly gc.start

--- lib/eye/process/watchers.rb
+++ lib/eye/process/watchers.rb
@@ -9,7 +9,6 @@ module Eye::Process::Watchers
       # default watcher :check_alive
       add_watcher(:check_alive, self[:check_alive_period]) do 
         check_alive
-        GC.start if rand(1000) == 0
       end
 
       # monitor childs pids
