f0cc8e9cdf3c781131ffb854a4abd21aec7e3f57
Konstantin Makarchev <kostya27@gmail.com>
2013-04-08 01:25:52 +0400

    specs for flapping retry_in, retry_times

--- spec/process/triggers/flapping_spec.rb
+++ spec/process/triggers/flapping_spec.rb
@@ -91,7 +91,6 @@ describe "Flapping" do
     proxy(@process).schedule(:check_crash, anything)
     dont_allow(@process).schedule(:unmonitor)
 
-
     sleep 2
 
     2.times do
@@ -102,6 +101,142 @@ describe "Flapping" do
     sleep 2
 
     @process.state_name.should == :up    
+  end
+
+  describe "retry_in, retry_times" do
+    before :each do
+      @c = C.p1.merge(
+        :triggers => C.flapping(:times => 2, :within => 3),
+        :start_grace => 0.1, # for fast flapping
+        :stop_grace => 0,
+        :start_command => @c[:start_command] + " -r"
+      )    
+    end
+
+    it "flapping than wait for interval and try again" do
+      @process = process(@c.merge(:triggers => C.flapping(:times => 2, :within => 3, 
+        :retry_in => 5.seconds)))
+      @process.start!
+
+      sleep 18
+
+      h = @process.states_history
+
+      # был в unmonitored
+      h.shift[:state].should == :unmonitored
+
+      # должен попытаться подняться два раза,
+      h.shift(6)
+
+      # затем перейти в unmonitored с причиной flapping
+      flapp1 = h.shift
+      flapp1[:state].should == :unmonitored
+      flapp1[:reason].should == 'flapping'
+
+      # затем снова попыться подняться два раза
+      h.shift(6)
+
+      # и снова перейти в unmonitored с причиной flapping
+      flapp2 = h.shift
+      flapp2[:state].should == :unmonitored
+      flapp2[:reason].should == 'flapping'
+
+      # интервал между переходами во flapping должен быть больше 8 сек
+      (flapp2[:at] - flapp1[:at]).should > 5.seconds
+
+      # тут снова должен пытаться подниматься так как нет лимитов
+      h.should_not be_blank
+    end
+
+    it "flapping retry 1 times with retry_times = 1" do
+      @process = process(@c.merge(:triggers => C.flapping(:times => 2, :within => 3, 
+        :retry_in => 5.seconds, :retry_times => 1)))
+      @process.start!
+
+      sleep 18
+
+      h = @process.states_history
+
+      # был в unmonitored
+      h.shift[:state].should == :unmonitored
+
+      # должен попытаться подняться два раза,
+      h.shift(6)
+
+      # затем перейти в unmonitored с причиной flapping
+      flapp1 = h.shift
+      flapp1[:state].should == :unmonitored
+      flapp1[:reason].should == 'flapping'
+
+      # затем снова попыться подняться два раза
+      h.shift(6)
+
+      # и снова перейти в unmonitored с причиной flapping
+      flapp2 = h.shift
+      flapp2[:state].should == :unmonitored
+      flapp2[:reason].should == 'flapping'
+
+      # интервал между переходами во flapping должен быть больше 8 сек
+      (flapp2[:at] - flapp1[:at]).should > 5.seconds
+
+      # все финал
+      h.should be_blank
+    end
+
+    it "flapping than manually doing something, should not retry" do
+      @process = process(@c.merge(:triggers => C.flapping(:times => 2, :within => 3, 
+        :retry_in => 5.seconds)))
+      @process.start!
+
+      sleep 6
+      @process.send_command :unmonitor
+      sleep 9
+
+      h = @process.states_history
+
+      # был в unmonitored
+      h.shift[:state].should == :unmonitored
+
+      # должен попытаться подняться два раза,
+      h.shift(6)
+
+      # затем перейти в unmonitored с причиной flapping
+      flapp1 = h.shift
+      flapp1[:state].should == :unmonitored
+      flapp1[:reason].should == 'flapping'
+
+      # затем его руками переводят в unmonitored
+      unm = h.shift
+      unm[:state].should == :unmonitored
+      unm[:reason].should == 'unmonitor by user'
+
+      # все финал
+      h.should be_blank
+    end
+
+    it "without retry_in" do
+      @process = process(@c.merge(:triggers => C.flapping(:times => 2, :within => 3)))
+      @process.start!
+
+      sleep 10
+
+      h = @process.states_history
+
+      # был в unmonitored
+      h.shift[:state].should == :unmonitored
+
+      # должен попытаться подняться два раза,
+      h.shift(6)
+
+      # затем перейти в unmonitored с причиной flapping
+      flapp1 = h.shift
+      flapp1[:state].should == :unmonitored
+      flapp1[:reason].should == 'flapping'
+
+      # все финал
+      h.should be_blank
+    end
+
   end
 
 end
