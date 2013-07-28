408a56772d7a5c4ab2d46fee483d5ed14ded6f6a
Konstantin Makarchev <kostya27@gmail.com>
2013-07-28 21:29:06 +0400

    refactor custom specs

--- spec/process/triggers/custom_spec.rb
+++ spec/process/triggers/custom_spec.rb
@@ -1,38 +1,86 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "Trigger Custom" do
+  before :each do
+    @c = Eye::Controller.new
+  end
+
   describe "delete file" do
     before :each do
-      @c = Eye::Controller.new
-      r = @c.load(fixture("dsl/custom_trigger1.eye"))
+      cfg = <<-D
+class DeleteFile < Eye::Trigger::Custom
+  param :file, [String], true
+  param :on, [Symbol]
+
+  def check(transition)
+    if transition.to_name == on
+      info "rm \#{file}"
+      File.delete(file)
+    end
+  end
+end
+
+Eye.application("bla") do
+  working_dir "#{C.sample_dir}"
+  process("1") do
+    pid_file "#{C.p1_pid}"
+    start_command "sleep 30"
+    daemonize true
+    trigger :delete_file, :file => "#{C.tmp_file}", :on => :down
+  end
+end
+      D
+
+      with_temp_file(cfg){ |f| @c.load(f) }
       sleep 5
       @process = @c.process_by_name("1")
-      @filename = @process[:working_dir] + "/1.tmp"
     end
 
     it "should delete file when stop" do
-      File.open(@filename, 'w'){ |f| f.write "aaa" }
-      File.exists?(@filename).should == true
+      File.open(C.tmp_file, 'w'){ |f| f.write "aaa" }
+      File.exists?(C.tmp_file).should == true
       @process.stop
-      File.exists?(@filename).should == false
+      File.exists?(C.tmp_file).should == false
     end
   end
 
   describe "delete file on event" do
     before :each do
-      @c = Eye::Controller.new
-      r = @c.load(fixture("dsl/custom_trigger2.eye"))
+      cfg = <<-D
+class DeleteFileEvent < Eye::Trigger::Custom
+  param :file, [String], true
+  param :on, [Symbol]
+
+  def check(transition)
+    if transition.event == on
+      info "rm \#{file}"
+      File.delete(file)
+    end
+  end
+end
+
+Eye.application("bla") do
+  working_dir "#{C.sample_dir}"
+  process("1") do
+    pid_file "#{C.p1_pid}"
+    start_command "sleep 30"
+    daemonize true
+    trigger :delete_file_event, :file => "#{C.tmp_file}", :on => :crashed
+  end
+end
+      D
+
+      with_temp_file(cfg){ |f| @c.load(f) }
       sleep 5
       @process = @c.process_by_name("1")
-      @filename = @process[:working_dir] + "/1.tmp"
     end
 
     it "should delete file when stop" do
-      File.open(@filename, 'w'){ |f| f.write "aaa" }
-      File.exists?(@filename).should == true
+      File.open(C.tmp_file, 'w'){ |f| f.write "aaa" }
+      File.exists?(C.tmp_file).should == true
       force_kill_pid(@process.pid)
       sleep 5
-      File.exists?(@filename).should == false
+      File.exists?(C.tmp_file).should == false
     end
   end
 
