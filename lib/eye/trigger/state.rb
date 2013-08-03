6de94b2b48b1c1ba47c94cdceb9223201362c98e
Konstantin Makarchev <kostya27@gmail.com>
2013-08-03 20:37:17 +0400

    state trigger allow multiple states

--- lib/eye/trigger/state.rb
+++ lib/eye/trigger/state.rb
@@ -2,16 +2,25 @@ class Eye::Trigger::State < Eye::Trigger
 
   # triggers :state, :to => :up, :from => :starting, :do => ->{ ... }
 
-  param :to, [Symbol]
-  param :from, [Symbol]
-  param :event, [Symbol]
+  param :to, [Symbol, Array]
+  param :from, [Symbol, Array]
+  param :event, [Symbol, Array]
   param :do, [Proc]
 
-  def check(transition)
-    if (to && transition.to_name == to) ||
-        (from && from == transition.from_name) ||
-        (event && transition.event == event)
+  def check(trans)
+    if compare(trans.to_name, to) || compare(trans.from_name, from) || compare(trans.event, event)
       @options[:do].call if @options[:do]
+    end
+  end
+
+private
+
+  def compare(state_name, condition)
+    case condition
+    when Symbol
+      state_name == condition
+    when Array
+      condition.include?(state_name)
     end
   end
 
