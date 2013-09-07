40a935a4f4c4aeb748b90cdfdac068035a577d32
Konstantin Makarchev <kostya27@gmail.com>
2013-09-07 20:51:23 +0400

    fix bin doc

--- lib/eye/loader.rb
+++ lib/eye/loader.rb
@@ -1,4 +1,5 @@
-# mini bundler, for embedded server gem installation
+# add gems to $: by `gem` method
+#  this is only way when install eye as system wide
 
 gem 'celluloid',     '~> 0.15.0'
 gem 'celluloid-io',  '~> 0.15.0'
