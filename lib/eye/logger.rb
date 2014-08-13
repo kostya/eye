52f72da2140c4979c8642cc29014b6feaea415f3
Konstantin Makarchev <kostya27@gmail.com>
2014-08-14 02:55:06 +0400

    fix logger block

--- lib/eye/logger.rb
+++ lib/eye/logger.rb
@@ -29,8 +29,8 @@ class Eye::Logger
 
     Logger::Severity.constants.each do |level|
       method_name = level.to_s.downcase
-      define_method method_name do |msg|
-        logger.send(method_name, msg)
+      define_method method_name do |msg = nil, &block|
+        logger.send(method_name, msg, &block)
       end
     end
 
@@ -42,8 +42,8 @@ class Eye::Logger
 
   Logger::Severity.constants.each do |level|
     method_name = level.to_s.downcase
-    define_method method_name do |msg|
-      self.class.inner_logger.send(method_name, "#{prefix_str}#{msg}")
+    define_method method_name do |msg = nil, &block|
+      self.class.inner_logger.send(method_name, "#{prefix_str}#{msg}", &block)
     end
   end
 
