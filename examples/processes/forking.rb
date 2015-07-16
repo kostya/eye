f8adf2a3be5d11e4b7057c871a335049877fb236
Konstantin Makarchev <kostya27@gmail.com>
2015-07-16 03:58:03 +0300

    add more children form forker example

--- examples/processes/forking.rb
+++ examples/processes/forking.rb
@@ -2,12 +2,13 @@ require 'bundler/setup'
 require 'forking'
 
 root = File.expand_path(File.dirname(__FILE__))
+cnt = (ENV['FORKING_COUNT'] || 3).to_i
 
 f = Forking.new(:name => 'forking', :working_dir => root,
     :log_file => "#{root}/forking.log",
     :pid_file => "#{root}/forking.pid", :sync_log => true)
 
-3.times do |i|
+cnt.times do |i|
   f.spawn(:log_file => "#{root}/child#{i}.log", :sync_log => true) do
     $0 = "forking child"
     loop do
