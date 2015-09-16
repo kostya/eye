3b284d3d270759975d84bc5be475a3de495e8035
Konstantin Makarchev <kostya27@gmail.com>
2015-09-16 03:06:59 +0300

    refactor check_pid_file method

--- lib/eye/process/monitor.rb
+++ lib/eye/process/monitor.rb
@@ -42,44 +42,46 @@ private
 
         switch :crashed, Eye::Reason.new(:crashed)
       else
-        # check that pid_file still here
-        ppid = failsafe_load_pid
-
-        if ppid != self.pid
-          msg = "check_alive: pid_file (#{self[:pid_file]}) changed by itself (<#{self.pid}> => <#{ppid}>)"
-          if control_pid?
-            msg += ", reverting to <#{self.pid}> (the pid_file is controlled by eye)"
-            unless failsafe_save_pid
-              msg += ", pid_file write failed! O_o"
-            end
-          else
-            changed_ago_s = Time.now - pid_file_ctime
-
-            if ppid == nil
-              msg += ", reverting to <#{self.pid}> (the pid_file is empty)"
-              unless failsafe_save_pid
-                msg += ", pid_file write failed! O_o"
-              end
-
-            elsif (changed_ago_s > self[:auto_update_pidfile_grace]) && process_pid_running?(ppid)
-              msg += ", trusting this change, and now monitor <#{ppid}>"
-              self.pid = ppid
-
-            elsif (changed_ago_s > self[:revert_fuckup_pidfile_grace])
-              msg += " over #{self[:revert_fuckup_pidfile_grace]}s ago, reverting to <#{self.pid}>, because <#{ppid}> not alive"
-              unless failsafe_save_pid
-                msg += ", pid_file write failed! O_o"
-              end
-
-            else
-              msg += ', ignoring self-managed pid change'
-            end
-          end
-
-          warn msg
-        end
+        check_pid_file
       end
     end
+  end
+
+  def check_pid_file
+    ppid = failsafe_load_pid
+    return if ppid == self.pid
+
+    msg = "check_alive: pid_file (#{self[:pid_file]}) changed by itself (<#{self.pid}> => <#{ppid}>)"
+    if control_pid?
+      msg += ", reverting to <#{self.pid}> (the pid_file is controlled by eye)"
+      unless failsafe_save_pid
+        msg += ", pid_file write failed! O_o"
+      end
+    else
+      changed_ago_s = Time.now - pid_file_ctime
+
+      if ppid == nil
+        msg += ", reverting to <#{self.pid}> (the pid_file is empty)"
+        unless failsafe_save_pid
+          msg += ", pid_file write failed! O_o"
+        end
+
+      elsif (changed_ago_s > self[:auto_update_pidfile_grace]) && process_pid_running?(ppid)
+        msg += ", trusting this change, and now monitor <#{ppid}>"
+        self.pid = ppid
+
+      elsif (changed_ago_s > self[:revert_fuckup_pidfile_grace])
+        msg += " over #{self[:revert_fuckup_pidfile_grace]}s ago, reverting to <#{self.pid}>, because <#{ppid}> not alive"
+        unless failsafe_save_pid
+          msg += ", pid_file write failed! O_o"
+        end
+
+      else
+        msg += ', ignoring self-managed pid change'
+      end
+    end
+
+    warn msg
   end
 
   def check_crash
