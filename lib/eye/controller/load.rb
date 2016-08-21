0de981b4b3c180eaf60f90b8ae3f86126b601ba9
Konstantin Makarchev <kostya27@gmail.com>
2016-08-22 00:24:09 +0300

    minimize change to races in controller#load

--- lib/eye/controller/load.rb
+++ lib/eye/controller/load.rb
@@ -114,17 +114,33 @@ private
     @applications.sort_by!(&:name)
   end
 
+  class AppDiff
+
+    attr_reader :old_groups, :old_processes, :added_groups, :added_processes
+
+    def initialize
+      @old_groups = {}
+      @old_processes = {}
+      @added_groups = []
+      @added_processes = []
+    end
+
+    def remove_added_processes(processes)
+      @added_processes -= processes
+    end
+
+  end
+
   def update_or_create_application(app_name, app_config)
-    @old_groups = {}
-    @old_processes = {}
+    diff = AppDiff.new
 
     app = @applications.detect { |c| c.name == app_name }
 
     if app
       app.groups.each do |group|
-        @old_groups[group.name] = group
+        diff.old_groups[group.name] = group
         group.processes.each do |proc|
-          @old_processes[group.name + ':' + proc.name] = proc
+          diff.old_processes[group.name + ':' + proc.name] = proc
         end
       end
 
@@ -136,85 +152,78 @@ private
     end
 
     app = Eye::Application.new(app_name, app_config)
-    @applications << app
-    @added_groups = []
-    @added_processes = []
 
     new_groups = app_config.delete(:groups) || {}
     new_groups.each do |group_name, group_cfg|
-      group = update_or_create_group(group_name, group_cfg.clone)
+      group = update_or_create_group(group_name, group_cfg.clone, diff)
       app.add_group(group)
       group.resort_processes
     end
 
-    # now, need to clear @old_groups, and @old_processes
-    @old_groups.each do |_, group|
+    diff.old_groups.each do |_, group|
       group.clear
       group.send_call(command: :delete, reason: 'load by user')
     end
-    @old_processes.each do |_, process|
+
+    diff.old_processes.each do |_, process|
       process.send_call(command: :delete, reason: 'load by user') if process.alive?
     end
 
     # schedule monitoring for new groups, processes
     added_fully_groups = []
-    @added_groups.each do |group|
-      if !group.processes.empty? && (group.processes.pure - @added_processes).empty?
+    diff.added_groups.each do |group|
+      if !group.processes.empty? && (group.processes.pure - diff.added_processes).empty?
         added_fully_groups << group
-        @added_processes -= group.processes.pure
+        diff.remove_added_processes(group.processes.pure)
       end
     end
 
     added_fully_groups.each { |group| group.send_call command: :monitor, reason: 'load by user' }
-    @added_processes.each { |process| process.send_call command: :monitor, reason: 'load by user' }
-
-    # remove links to prevent memory leaks
-    @old_groups = nil
-    @old_processes = nil
-    @added_groups = nil
-    @added_processes = nil
+    diff.added_processes.each { |process| process.send_call command: :monitor, reason: 'load by user' }
 
     app.resort_groups
+
+    @applications << app
 
     app
   end
 
-  def update_or_create_group(group_name, group_config)
-    group = if @old_groups[group_name]
+  def update_or_create_group(group_name, group_config, diff)
+    group = if diff.old_groups[group_name]
       debug { "updating group: #{group_name}" }
-      group = @old_groups.delete(group_name)
+      group = diff.old_groups.delete(group_name)
       group.send_call command: :update_config, args: [group_config], reason: 'load by user'
       group.clear
       group
     else
       debug { "creating group: #{group_name}" }
       gr = Eye::Group.new(group_name, group_config)
-      @added_groups << gr
+      diff.added_groups << gr
       gr
     end
 
     processes = group_config.delete(:processes) || {}
     processes.each do |process_name, process_cfg|
-      process = update_or_create_process(process_name, process_cfg.clone)
+      process = update_or_create_process(process_name, process_cfg.clone, diff)
       group.add_process(process)
     end
 
     group
   end
 
-  def update_or_create_process(process_name, process_cfg)
+  def update_or_create_process(process_name, process_cfg, diff)
     postfix = ':' + process_name
     name = process_cfg[:group] + postfix
-    key = @old_processes[name] ? name : @old_processes.keys.detect { |n| n.end_with?(postfix) }
+    key = diff.old_processes[name] ? name : diff.old_processes.keys.detect { |n| n.end_with?(postfix) }
 
-    if @old_processes[key]
+    if diff.old_processes[key]
       debug { "updating process: #{name}" }
-      process = @old_processes.delete(key)
+      process = diff.old_processes.delete(key)
       process.send_call command: :update_config, args: [process_cfg], reason: 'load by user'
     else
       debug { "creating process: #{name}" }
       process = Eye::Process.new(process_cfg)
-      @added_processes << process
+      diff.added_processes << process
     end
 
     process
