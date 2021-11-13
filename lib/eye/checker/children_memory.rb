f8d62fe76dac1e75437d26caa418cf9035d8d626
Damir Sharipov <dammer2k@gmail.com>
2021-11-13 09:48:19 +0300

    fix(log): add human readable values to children_memory log

--- lib/eye/checker/children_memory.rb
+++ lib/eye/checker/children_memory.rb
@@ -3,10 +3,18 @@ class Eye::Checker::ChildrenMemory < Eye::Checker::Measure
   # check :children_memory, :every => 30.seconds, :below => 400.megabytes
   #   monitor_children should be enabled
 
+  def check_name
+    @check_name ||= "children_memory(#{measure_str})"
+  end
+
   def get_value
     process.children.values.inject(0) do |sum, ch|
       sum + Eye::SystemResources.memory(ch.pid).to_i
     end
+  end
+
+  def human_value(value)
+    "#{value.to_i / 1024 / 1024}Mb"
   end
 
 end
